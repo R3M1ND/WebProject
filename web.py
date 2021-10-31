d3fd9c2b99463fda68c6ae9844072fa1a6cce53b
naththanicha ardsuwan <naththanicha@gmail.com>
2021-10-31 21:49:19 +0700

    linear

--- web.py
+++ web.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def Home():
     return render_template('web.html')
-
+@app.route('/my-link/')
+def my_link():
+    return render_template('rankweb.html')
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
