18990c5a7a8fedfed58264831313b2dc2d28a9a7
naththanicha ardsuwan <naththanicha@gmail.com>
2021-11-02 19:38:41 +0700

    linear

--- web.py
+++ web.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
+#---------------------------------------------------------------------
 @app.route('/')
 def Home():
     return render_template('web.html')
 @app.route('/my-link/')
 def my_link():
     return render_template('rankweb.html')
+@app.route('/searchengine',methods = ['POST',"GET"])
+def search():
+    output = request.form.to_dict()
+    search = output["search"]   
+    return render_template('rankweb.html',search = search)
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
